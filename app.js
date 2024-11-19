75578e98fa884a28ba077335023b68d51bc7c973
Ashish0253 <ashishrana0253@gmail.com>
2024-11-19 11:15:44 +0530

    adding custom css url

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ const swaggerSpecs = require('./swagger');
 
 dotenv.config();
 
+
 const app = express();
 
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs, {customCssUrl: process.env.CSS_URL}));
 
 app.use(express.json());
 app.use(cors());
